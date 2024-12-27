50b042865fc240245b0593acf5a337e2e1e90bec
GiLonga <giorgio.longari@mail.polimi.it>
2024-12-27 07:57:17 +0100

    added keypoints and a doc string for the class patient.

--- src/TMIgeometry.py
+++ src/TMIgeometry.py
@@ -1,6 +1,23 @@
 
 
 class Patient():
+    """Patient Class.
+
+    Parameters
+    ----------
+    mesh : array-like, shape=[n_vertices, 3]
+        Processed mesh of the patient.
+    keypoints : array-like, shape=[N,3]
+        Keypoints useful to obtain the isocenters.
+    or_isocenters : array-like, shape=[M,3]
+        Original isocenters in the plan.
+    or_fields : array-like, shape=[M,3]
+        Original fields in the plan.
+    p2p : array-like, shape=[V]
+        Point to point map between the template and the Patient
+    template: Bool,
+        To set as true if the instance is the template.
+    """
     def init(self, PTV, template=False):
         self.mesh = PTV
         self.keypoints = self.set_keypoints()
@@ -14,7 +31,21 @@ class Patient():
         """
         A pre determinated set of keypoints, necessary to calculate the isocenters coordinates.
         """
-        return
+        if self.template:
+            keypoints = [ 9522, 11032, 10203, 10368,  9766,  9789, 10396, 10560, 10132, 9822, 5751, 15264, 10678, 10621, # maximum points head
+              10397, #head low field
+              1039, 19844, #shoulder's length
+              10149, #second iso
+              8529, #third iso
+              8828, #fourth iso
+              623, 19509, #iso on the arms
+              3196, 16180, #heigth arm field
+              5, #length arm field
+              2972,  4055, 16249,  4330, 16175, 17162,  4532,  3627,  3625, 2971, #feet as minimum point of PTV
+              ]
+        else:
+            None
+        return keypoints
     
     def load_isocenters(self,):
         """
@@ -34,9 +65,9 @@ class Patient():
         """
         Return the p2p calculated in the spectral section.
         """
-        if self.template == True:
+        if self.template:
             ValueError("You can't calculate a point to point map for the Template. Check it!")
-            
+
         return
     
     def calculate_keypoints(self,):
